bbca48f065e4dc865bcf89863b286834d53305f0
Alexey <redspirit2@ya.ru>
2019-06-25 00:03:51 +0300

    update

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,17 +3,22 @@ extends KinematicBody2D
 export (int) var anim_frame = 0
 
 var frames = [
+	#stay
 	Rect2(8, 9, 22, 32),
+	#walk
 	Rect2(80, 10, 15, 31),
 	Rect2(101, 10, 17, 31),
 	Rect2(124, 9, 15, 32),
-	Rect2(146, 10, 17, 31)
+	Rect2(146, 10, 17, 31),
+	#beat
+	Rect2(4, 135, 23, 31),
+	Rect2(39, 135, 31, 31)
 ]
 var flip = false
 var motionX = 0
 var motionY = 0
-var speedX = 50
-var speedY = 30
+var speedX = 200
+var speedY = 150
 
 
 func _ready():
@@ -21,7 +26,7 @@ func _ready():
 	pass
 
 
-func _process(delta):
+func _physics_process(delta):
 	
 	
 	if Input.is_action_pressed("ui_up"):
@@ -49,5 +54,14 @@ func _process(delta):
 	
 	$cody.flip_h = flip
 	$cody.region_rect = frames[anim_frame]
-	$cody.position.x += motionX * delta * speedX
-	$cody.position.y += motionY * delta * speedY
+
+	move_and_slide(Vector2(motionX * speedX, motionY * speedY))
+	
+func _input(event):
+	
+	if event.is_action_pressed("ui_accept"):
+		#print("action")
+		anim_frame = 5
+	
+	
+	pass
