f85c2a1205076394b3357cfa7de6b1fc7dc26d3e
Alexey <redspirit2@ya.ru>
2019-06-28 17:21:37 +0300

    update

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,6 +5,7 @@ var speed = 200
 
 var motion = Vector2(0, 0)
 var dir = 1
+var isFight = false
 
 func _ready():
 	$anim.play("stay-right")
@@ -13,21 +14,21 @@ func _ready():
 
 func _physics_process(delta):
 	
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up") && !isFight:
 		motion.y = -1
 		walk()
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("ui_down") && !isFight:
 		motion.y = 1
 		walk()
 	else :
 		motion.y = 0
 		#stay()
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") && !isFight:
 		motion.x = 1
 		dir = 1
 		walk()
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left") && !isFight:
 		motion.x = -1
 		dir = -1
 		walk()
@@ -67,15 +68,22 @@ func beat():
 	if dir > 0:
 		$anim.play("beat-right")
 	if dir < 0:
-		$anim.play("beat-left")	
+		$anim.play("beat-left")
 	
 func _input(event):
 	
 
 	if event.is_action_pressed("beat"):
+		isFight = true
 		beat()
 		
 	if event.is_action_released("beat"):
+		isFight = false
 		stay()
+		
+	if event.is_action_pressed("jump"):
+		print("JUMP")
+		
+	if event.is_action_released("jump"):
 		pass
 
